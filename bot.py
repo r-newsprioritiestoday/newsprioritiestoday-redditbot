efacc4003f380b3733763dee6e5261e51887a4d0
G710 <16511117+G710@users.noreply.github.com>
2019-03-02 10:26:07 +0100

    first reddit bot version working

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import praw
 from tinydb import TinyDB, Query, where
 from tinydb_serialization import Serializer, SerializationMiddleware
-from datetime import datetime
+from datetime import datetime, timedelta
+
+# setup serializer, database and reddit bot
 
 class DateTimeSerializer(Serializer):
     OBJ_CLASS = datetime  # The class this serializer handles
@@ -20,14 +22,35 @@ db = TinyDB('../data/db.json', storage=serialization)
 reddit = praw.Reddit('bot', user_agent='newsprioritytoday:test user agent')
 
 
-print(db.search(where('datetime') < datetime.now()))
-
-newssummary
 
 # get data from database and generate text
 
 
 
+# get data that was created less than an hour ago.
+# ideally the scraper and the bot should run at different times to avoid any result overlapping
+results = db.search(where('datetime') > (datetime.now() - timedelta(hours = 1)))
+
+# create reddit post
+newssummary = "Here are your daily Top 5 news from all over the world: \n\n"
+newssummary += "All news are kept in their original language. To translate to english, right click on a sentence an select 'Translate to english' (in chrome) \n\n"
+
+for result in results:
+    newssummary += "## " + result["country"] + "\n"
+
+    for article in result["articles"][:5]:
+        newssummary += "- " + article["headline"]
+        if article["text"] != "":
+            newssummary += " - " + article["text"]
+
+        newssummary += "\n"
+    
+    newssummary += "\n^(Source: " + result["source"] + " @ " + str(result["datetime"]) + ")"
+    newssummary += "\n\n"
+    
+
+
+print(newssummary)
 # post the info on the subreddit
 subreddit = reddit.subreddit('reddit_api_test')
 
